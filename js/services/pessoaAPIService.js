ca1693995d41e016d8cefe78428c315305a965f1
rpdeb <deboraribeiro9015@gmail.com>
2023-02-26 04:22:56 -0300

    Create pessoaAPIService.js

---
+++ js/services/pessoaAPIService.js
@@ -0,0 +1,24 @@
+angular.module("pessoaController").factory("pessoasAPI", function ($http, config) {
+	var _buscar = function () {
+		return $http.get(config.baseUrl + "/pessoas");
+	};
+
+	var _adicionar = function (pessoa) {
+		return $http.post(config.baseUrl + "/pessoas", pessoa);
+	};
+
+    var _alterar = function (pessoa) {
+		return $http.put(config.baseUrl + `/pessoas/${pessoa.id}`, pessoa);
+	};
+
+    var _excluir = function (pessoa) {
+		return $http.delete(config.baseUrl + `/pessoas/${pessoa.id}`);
+	};
+
+	return {
+		buscar: _buscar,
+		adicionar: _adicionar,
+        alterar: _alterar,
+        excluir: _excluir
+	};
+});
