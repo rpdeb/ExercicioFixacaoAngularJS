354bd6b119bc083bd8ffa82ced7554297e0e40ac
Debora Ribeiro Pereira <deboraribeiro9015@gmail.com>
2023-02-27 16:56:17 -0300

    Update compraAPIService.js

--- js/services/compraAPIService.js
+++ js/services/compraAPIService.js
@@ -1,24 +1,24 @@
-angular.module("compra").factory("comprasAPI", function ($http) {
-	var _buscar = function () {
-		return $http.get("http://localhost:3000/compras");
-	};
+angular.module("aplicacao").factory("comprasAPI", function ($http) {
+    var _buscar = function () {
+        return $http.get(config.baseUrl + "/compras");
+    };
 
-	var _adicionar = function (compra) {
-		return $http.post("http://localhost:3000/compras", compra);
-	};
+    var _adicionar = function (compra) {
+        return $http.post(config.baseUrl + "/compras", compra);
+    };
 
     var _alterar = function (compra) {
-		return $http.put(`http://localhost:3000/compras/${compra.id}`, compra);
-	};
+        return $http.put(config.baseUrl + `/compras/${compra.id}`, compra);
+    };
 
     var _excluir = function (compra) {
-		return $http.delete(`http://localhost:3000/compras/${compra.id}`,compra);
-	};
+        return $http.delete(config.baseUrl + `/compras/${compra.id}`, compra);
+    };
 
-	return {
-		buscar: _buscar,
-		adicionar: _adicionar,
+    return {
+        buscar: _buscar,
+        adicionar: _adicionar,
         alterar: _alterar,
         excluir: _excluir
-	};
+    };
 });
