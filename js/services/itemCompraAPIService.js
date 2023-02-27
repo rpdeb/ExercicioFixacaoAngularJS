e0f5c32e2535a4b30b985831c679822313d3584e
rpdeb <deboraribeiro9015@gmail.com>
2023-02-27 03:59:39 -0300

    Create itemCompraAPIService.js

---
+++ js/services/itemCompraAPIService.js
@@ -0,0 +1,24 @@
+angular.module("itemCompra").factory("itensAPI", function ($http, config) {
+	var _buscar = function () {
+		return $http.get(config.baseUrl + "/itensdecompra");
+	};
+
+	var _adicionar = function (item) {
+		return $http.post(config.baseUrl + "/itensdecompra", item);
+	};
+
+    var _alterar = function (item) {
+		return $http.put(config.baseUrl + `/itensdecompra/${item.id}`, item);
+	};
+
+    var _excluir = function (item) {
+		return $http.delete(config.baseUrl + `/itensdecompra/${item.id}`,item);
+	};
+
+	return {
+		buscar: _buscar,
+		adicionar: _adicionar,
+        alterar: _alterar,
+        excluir: _excluir
+	};
+});
