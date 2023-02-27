5c110b338d8c2a663d040a7a942b5daf57fef2c5
rpdeb <deboraribeiro9015@gmail.com>
2023-02-27 03:59:10 -0300

    Update itemCompraController.js
    ajuste no nome dos métodos e chamamento do service

--- js/controllers/itemCompraController.js
+++ js/controllers/itemCompraController.js
@@ -1,56 +1,41 @@
-angular.module("crudItemCompra", []);
-angular.module("crudItemCompra").controller("itemCompraCtrl", function ($scope, $http) {
+angular.module("itemCompra", []);
+angular.module("itemCompra").controller("itemCompraController", function ($scope, $http) {
 
     $scope.novoItem = {};
     $scope.itemSelecionado = {};
     $scope.itens = [];
-    $scope.compras = [];
     $scope.novoItem.codigo = Math.floor(Math.random() * 10000);
   
-    var listarItens = function () {
-        $http.get("http://localhost:3000/itensdecompra").success(function (data) {
-            console.log(data, "itens");
+    var carregarItens = function () {
+         itensAPI.buscar().success(function (data) {
             $scope.itens = data;
         });
     };
 
-    $scope.salvar = function () {
-        $scope.itens.push($scope.novoItem);
-        $http.post("http://localhost:3000/itensdecompra", $scope.novoItem).success(function (data) {
-		console.log("realizou o post - pessoa com id", data);	
-        listarItens();
+    $scope.adicionarItem = function (item) {
+        var item = $scope.novoItem;
+        itensAPI.adicionar(item).success(function (data) {	
+        carregarItens();
 		});
-        //$scope.novoItem = {};
     };
 
-    $scope.selecionaItem = function (produto) {
-        $scope.itemSelecionado = produto;
+    $scope.selecionaItem = function (item) {
+        $scope.itemSelecionado = item;
     };
 
     $scope.alterarItem = function () {
-        var itemCompra = $scope.itemSelecionado;
-
-        $http.put(`http://localhost:3000/itensdecompra/${itemCompra.id}`,itemCompra).success(function (data) {
-			$scope.itens = data;
-			console.log($scope.itens);	
-			console.log("sucesso na alteração");
+        var item = $scope.itemSelecionado;
+        itensAPI.alterar(item).success(function (data) {
+            carregarItens();
 			});
-        listarItens();
+       
     };
 
     $scope.excluirItem = function () {
-        var itemCompra = $scope.itemSelecionado;
-        console.log("excluir");
-        
-        $http.delete(`http://localhost:3000/itensdecompra/${itemCompra.id}`,itemCompra).success(function (data) {
-			//$scope.itens = data;
-			console.log($scope.itens);	
-			console.log("sucesso na exclusão");
-            listarItens();
+        var item = $scope.itemSelecionado;
+        itensAPI.excluir(item).success(function (data) {
+            carregarItens();
 			});
-        $scope.itens.splice($scope.itens.indexOf($scope.itemSelecionado), 1);
     };
-
-    listarItens();
-    //listarCompras();
+    carregarItens();
 });
