eb8a81084886896aa7edc6cc7e6557a223b5c1a0
Debora Ribeiro Pereira <deboraribeiro9015@gmail.com>
2023-02-27 16:56:20 -0300

    Update itemCompraAPIService.js

--- js/services/itemCompraAPIService.js
+++ js/services/itemCompraAPIService.js
@@ -1,24 +1,24 @@
-angular.module("itemCompra").factory("itensAPI", function ($http) {
-	var _buscar = function () {
-		return $http.get("http://localhost:3000/itensdecompra");
-	};
+angular.module("aplicacao").factory("itensAPI", function ($http) {
+    var _buscar = function () {
+        return $http.get(config.baseUrl + "/itensdecompra");
+    };
 
-	var _adicionar = function (item) {
-		return $http.post("http://localhost:3000/itensdecompra", item);
-	};
+    var _adicionar = function (item) {
+        return $http.post(config.baseUrl + "/itensdecompra", item);
+    };
 
     var _alterar = function (item) {
-		return $http.put(`http://localhost:3000/itensdecompra/${item.id}`, item);
-	};
+        return $http.put(config.baseUrl + `/itensdecompra/${item.id}`, item);
+    };
 
     var _excluir = function (item) {
-		return $http.delete(`http://localhost:3000/itensdecompra/${item.id}`,item);
-	};
+        return $http.delete(config.baseUrl + `/itensdecompra/${item.id}`, item);
+    };
 
-	return {
-		buscar: _buscar,
-		adicionar: _adicionar,
+    return {
+        buscar: _buscar,
+        adicionar: _adicionar,
         alterar: _alterar,
         excluir: _excluir
-	};
+    };
 });
