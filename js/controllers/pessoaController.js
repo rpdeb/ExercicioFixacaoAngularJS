b61bea7ae3dc48c6cfaa5ab5f151fda59b151d95
Debora Ribeiro Pereira <deboraribeiro9015@gmail.com>
2023-03-03 15:03:57 -0300

    Implementando service pessoa

--- js/controllers/pessoaController.js
+++ js/controllers/pessoaController.js
@@ -1,22 +1,22 @@
 angular.module("aplicacao", []);
-angular.module("aplicacao").controller("pessoaController", function ($scope, $http) {
+angular.module("aplicacao").controller("pessoaController", function ($scope, $http, pessoasAPI) {
 
     $scope.novaPessoa = {};
     $scope.pessoaSelecionado = {};
     $scope.pessoas = [];
     $scope.novaPessoa.codigo = Math.floor(Math.random() * 1000);
 
-    var carregarPessoas = function () {
-        $http.get("http://localhost:3000/pessoas").success(function (data) {
+    var buscarPessoas = function () {
+        pessoasAPI.buscar().success(function (data) {
             $scope.pessoas = data;
         });
     };
 
-    $scope.adicionarPessoa = function () {
+    $scope.cadastrarPessoa = function () {
         var pessoa = $scope.novaPessoa;
         pessoa.nascimento = new Date();
-        $http.post("http://localhost:3000/pessoas", pessoa).success(function (data) {
-            carregarPessoas();
+        pessoasAPI.cadastrar(pessoa).success(function (data) {
+            buscarPessoas();
         });
     };
 
@@ -26,16 +26,16 @@ angular.module("aplicacao").controller("pessoaController", function ($scope, $ht
 
     $scope.alterarPessoa = function () {
         var pessoa = $scope.pessoaSelecionado;
-        $http.put(`http://localhost:3000/pessoas/${pessoa.id}`, pessoa).success(function (data) {
-            carregarPessoas();
+        pessoasAPI.alterar(pessoa).success(function (data) {
+            buscarPessoas();
         });
     };
 
     $scope.excluirPessoa = function () {
         var pessoa = $scope.pessoaSelecionado;
-        $http.delete(`http://localhost:3000/pessoas/${pessoa.id}`, pessoa).success(function (data) {
-            carregarPessoas();
+        pessoasAPI.excluir(pessoa).success(function (data) {
+            buscarPessoas();
         });
     };
-    carregarPessoas();
+    buscarPessoas();
 });
