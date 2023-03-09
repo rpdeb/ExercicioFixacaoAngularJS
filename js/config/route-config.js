5aabd5ebcdacca5d6d15b0a986668697960ab09c
Debora Ribeiro Pereira <deboraribeiro9015@gmail.com>
2023-03-09 16:30:37 -0300

    Update route-config.js

--- js/config/route-config.js
+++ js/config/route-config.js
@@ -1,38 +1,21 @@
-angular.module("aplicacao").config(function ($routeProvider) {
-
-    $routeProvider.when("/pessoa", {
-        templateUrl: "../view/pessoa.html",
-        controller: "pessoaController",
-        resolve: {
-            pessoas: function (pessoasAPI) {
-                return pessoasAPI.buscar();
-            }
-        }
-    });
-
-    $routeProvider.when("/item", {
-        templateUrl: "../view/item-compra.html",
-        controller: "itemCompraController",
-        resolve: {
-            itensdecompra: function (itensAPI) {
-                return itensAPI.buscar();
-            }
-        }
-    });
-
-    $routeProvider.when("/compra", {
-        templateUrl: "../view/compra.html",
-        controller: "compraController",
-        resolve: {
-            compras: function (comprasAPI) {
-                return comprasAPI.buscar();
-            }
-        }
-    });
-
-    $routeProvider.when("/error", {
-        templateUrl: "view/error.html"
-    });
-
-    $routeProvider.otherwise({ redirectTo: "/pessoa" });
+angular.module('aplicacao').config(function ($routeProvider) {
+    $routeProvider
+        .when('/produtos', {
+            templateUrl: 'view/item-compra.html',
+            controller: 'item-compra'
+        })
+        .when('/compra', {
+            templateUrl: 'view/compra.html',
+            controller: 'compra'
+        })
+        .when('/pessoa', {
+            templateUrl: 'view/pessoa.html',
+            controller: 'pessoa'
+        })
+        // .when('/home', {
+        //     templateUrl: 'index.html',
+        // })
+        .otherwise({
+            redirectTo: '/'
+        });
 });
